a40346a417f4c676bd86dae60cb9191ee5c1a507
jeremyben-meir <jsb459@cornell.edu>
2021-09-22 20:44:18 -0400

    toggleable views

--- src/components/Map.js
+++ src/components/Map.js
@@ -11,10 +11,10 @@ export const Map = () => {
 
     const mapContainer = useRef()
     const [hover, setHover] = useState({})
-    const map_focus_list = ['Duration', 'NAICS', 'Vacancy', 'Turnover']
-    const map_focus = map_focus_list[2]
+    const [mapFocus, setMapFocus] = useState("Vacancy")
+    const [mapSet, setMapSet] = useState()
 
-    const map_focus_dict = {
+    const colorDict = {
         "0" : '#00ff00',
         "1" : '#30ff00',
         "2" : '#60ff00',
@@ -27,7 +27,7 @@ export const Map = () => {
         "9" : '#ff3000',
         "10" : '#ff0000'
     }
-
+    
     const naics_string_dict = {
         "0" : 'Raw Goods Producers',
         "1" : 'Raw Goods Processors',
@@ -54,7 +54,7 @@ export const Map = () => {
         "9" : '9-10',
         "10" : '10+'
     }
-
+    
     const turnover_string_dict = {
         "0" : '<(.8)',
         "1" : '(.8)-(.6)',
@@ -67,7 +67,7 @@ export const Map = () => {
         "8" : '.6-.8',
         "9" : '>.8',
     }
-
+    
     const vacancy_string_dict = {
         "0" : '0-.1',
         "1" : '.1-.2',
@@ -80,134 +80,7 @@ export const Map = () => {
         "8" : '.8-.9',
         "9" : '.9-1',
     }
-
-    const popup_style = {
-        maxWidth: "400px",
-        font: "12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif"
-    }
-
-    var map_string_dict = {}
-
-    if (map_focus == "NAICS"){
-        map_string_dict = naics_string_dict
-    } else if (map_focus == "Duration"){
-        map_string_dict = duration_string_dict
-    } else if (map_focus == "Vacancy"){
-        map_string_dict = vacancy_string_dict
-    } else if (map_focus == "Turnover"){
-        map_string_dict = turnover_string_dict
-    }
-
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/light-v10',
-            zoom: 10.5,
-            maxZoom: 15.5,
-            minZoom: 8,
-            center: [-73.962141,40.72796]
-        });
-
-        
-        var place_data = {}
-        if (map_focus == "NAICS" || map_focus == "Duration"){
-            place_data = LLIDPlaces;
-        } else {
-            place_data = BBLPlaces;
-        }
-        console.log(place_data.default)
-        map.on('load', () => {
-
-            map.addSource('places', {
-                type: 'geojson',
-                data: place_data.default
-            });
-
-            map.addLayer({
-                'id': 'places',
-                'type': 'circle',
-                'source': 'places',
-                // 'source-layer': 'sf2010',
-                'paint': {
-                    // Make circles larger as the user zooms from z12 to z22.
-                    'circle-radius': {
-                        'base': 1.75,
-                        'stops': [
-                            [12, 2],
-                            [22, 50]
-                        ]
-                    },
-                    // Color circles by ethnicity, using a `match` expression.
-                    'circle-color': [
-                        'match',
-                        ['get', map_focus],
-                        "0", map_focus_dict['0'],
-                        "1", map_focus_dict['1'],
-                        "2", map_focus_dict['2'],
-                        "3", map_focus_dict['3'],
-                        "4", map_focus_dict['4'],
-                        "5", map_focus_dict['5'],
-                        "6", map_focus_dict['6'],
-                        "7", map_focus_dict['7'],
-                        "8", map_focus_dict['8'],
-                        "9", map_focus_dict['9'],
-                        /* other */ map_focus_dict['10']
-                    ]
-                }
-            });
-
-            // Create a popup, but don't add it to the map yet.
-            var popup = new mapboxgl.Popup({
-                offset: [0, -7],
-                closeButton: false,
-                closeOnClick: false
-            });
-                
-            map.on('mouseenter', 'places', (e) => {
-                // Change the cursor style as a UI indicator.
-                map.getCanvas().style.cursor = 'pointer';
-                
-                // Copy coordinates array.
-                const coordinates = e.features[0].geometry.coordinates.slice();
-
-                if (map_focus == "NAICS" || map_focus == "Duration"){
-                    const name = e.features[0].properties.Name;
-                    const address = e.features[0].properties.Address;
-                    const industry = e.features[0].properties["NAICS Title"];
-                    const start = e.features[0].properties["Start Date"];
-                    const end = e.features[0].properties["End Date"];
-
-                    setHover({
-                        "name": name,
-                        "address": address,
-                        "industry": industry,
-                        "start": start,
-                        "end": end,
-                    })
-                } else {
-                    const bbl = e.features[0].properties.BBL;
-                    const vacancy = e.features[0].properties.vacancy;
-                    const turnover = e.features[0].properties.turnover;
-                    const maxbus = e.features[0].properties["Max Business"];
-
-                    setHover({
-                        "bbl": bbl,
-                        "vacancy": vacancy,
-                        "turnover": turnover,
-                        "maxbus": maxbus
-                    })
-                }
-                
-            });
-                
-            map.on('mouseleave', 'places', () => {
-                map.getCanvas().style.cursor = '';
-                popup.remove();
-            });
-        });
-            
-    },[])
-
+    
     const legend_style = {
         backgroundColor: '#fff',
         borderRadius: '3px',
@@ -230,17 +103,161 @@ export const Map = () => {
         left: "10px",
         zIndex: 1
     }
-
+    
     const legend_h4_style = {
         margin: "0 0 10px"
     }
-
+    
     const legend_div_span_style = {
         borderRadius: "50%",
         display: "inline-block",
         height: "10px",
         marginRight: "5px",
         width: "10px"
+    }
+    
+    const mapFocusDict = {
+        'Duration':{
+            "strings": duration_string_dict,
+            "places": LLIDPlaces
+        },
+        'Vacancy':{
+            "strings": vacancy_string_dict,
+            "places": BBLPlaces
+        },
+        // 'NAICS':{
+        //     "strings": naics_string_dict,
+        //     "places": LLIDPlaces
+        // },
+        // 'Turnover':{
+        //     "strings": turnover_string_dict,
+        //     "places": BBLPlaces
+        // }
+    }
+
+    const update_map = () => {
+        const map = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/light-v10',
+            zoom: 10.5,
+            maxZoom: 15.5,
+            minZoom: 8,
+            center: [-73.962141,40.72796]
+        });
+
+        map.on('load', () => {
+
+            for(let [key, value] of Object.entries(mapFocusDict)){
+
+                map.addSource('places'+key, {
+                    type: 'geojson',
+                    data: mapFocusDict[key]["places"].default
+                });
+
+                map.addLayer({
+                    'id': key,
+                    'type': 'circle',
+                    'source': 'places'+key,
+                    'layout': {
+                        'visibility': key==mapFocus ? 'visible' : 'none',
+                    },
+                    'paint': {
+                        'circle-radius': {
+                            'base': 1.75,
+                            'stops': [
+                                [12, 2],
+                                [22, 50]
+                            ]
+                        },
+                        'circle-color': [
+                            'match',
+                            ['get', key],
+                            "0", colorDict['0'],
+                            "1", colorDict['1'],
+                            "2", colorDict['2'],
+                            "3", colorDict['3'],
+                            "4", colorDict['4'],
+                            "5", colorDict['5'],
+                            "6", colorDict['6'],
+                            "7", colorDict['7'],
+                            "8", colorDict['8'],
+                            "9", colorDict['9'],
+                            /* other */ colorDict['10']
+                        ]
+                    }
+                });
+                var popup = new mapboxgl.Popup({
+                    offset: [0, -7],
+                    closeButton: false,
+                    closeOnClick: false
+                });
+                    
+                map.on('mouseenter', key, (e) => {
+                    map.getCanvas().style.cursor = 'pointer'; // Change the cursor style as a UI indicator.
+                    if (key == "NAICS" || key == "Duration"){
+                        const name = e.features[0].properties.Name;
+                        const address = e.features[0].properties.Address;
+                        const industry = e.features[0].properties["NAICS Title"];
+                        const start = e.features[0].properties["Start Date"];
+                        const end = e.features[0].properties["End Date"];
+    
+                        setHover({
+                            "name": name,
+                            "address": address,
+                            "industry": industry,
+                            "start": start,
+                            "end": end,
+                        })
+                    } else {
+                        const bbl = e.features[0].properties.BBL;
+                        const vacancy = e.features[0].properties.vacancy;
+                        const turnover = e.features[0].properties.turnover;
+                        const maxbus = e.features[0].properties["Max Business"];
+    
+                        setHover({
+                            "bbl": bbl,
+                            "vacancy": vacancy,
+                            "turnover": turnover,
+                            "maxbus": maxbus
+                        })
+                    }
+                    
+                });
+                    
+                map.on('mouseleave', key, () => {
+                    map.getCanvas().style.cursor = '';
+                    popup.remove();
+                });
+
+            }
+        });
+        map.on('idle', () => {
+            setMapSet(map)
+        });
+    }
+
+    useEffect(() => {
+        update_map()            
+    },[])
+
+    useEffect(() => {
+        if (mapSet != null){
+            for(let [key, value] of Object.entries(mapFocusDict)){
+                if (key != mapFocus) {
+                    mapSet.setLayoutProperty(key, 'visibility', 'none');
+                } else {
+                    mapSet.setLayoutProperty(key, 'visibility','visible');
+                }       
+            }      
+        }
+    },[mapFocus])
+
+    const handleChange = (event) => {
+        if (mapFocus ==  "Vacancy"){
+            setMapFocus("Duration");
+        } else {
+            setMapFocus("Vacancy");
+        }
     }
 
     const llid_htm = (
@@ -264,16 +281,22 @@ export const Map = () => {
         <div> 
             <div ref={mapContainer} style={{ width: "100vw", height: "100vh" }}>
                 <div id="state-legend" className="legend" style={legend_style}>
-                    <h4 style={legend_h4_style}>{map_focus}</h4>
-                    {Object.entries(map_string_dict).map((item, index) => 
-                        <div><span style={{...legend_div_span_style, backgroundColor: map_focus_dict[index]}}></span>{map_string_dict[index]}</div>
+                    <h4 style={legend_h4_style}>{mapFocus}</h4>
+                    {Object.entries(mapFocusDict[mapFocus]["strings"]).map((item, index) => 
+                        <div><span style={{...legend_div_span_style, backgroundColor: colorDict[index]}}></span>{mapFocusDict[mapFocus]["strings"][index]}</div>
                     )}
                 </div>
                 <div id="state-legend" className="legend" style={legend_style_left}>
-                    {(map_focus == "Vacancy" || map_focus == "Turnover") ? bbl_htm : llid_htm}
+                    {(mapFocus == "Vacancy" || mapFocus == "Turnover") ? bbl_htm : llid_htm}
+                    <input type="checkbox" className="toggle-switch-checkbox" name="checkbox" id="checkbox" onChange={handleChange} /> Vacancy / Duration
                 </div>
             </div>
         </div>
     )
 
+
+    
 }
+
+
+
--- src/views/Team.js
+++ src/views/Team.js
@@ -5,18 +5,40 @@ import jeremy from '../assets/team/jeremy.png';
 
 export default function Map() {
 
+    var memberList = {
+        "Jeremy": {
+            "lastname": "Ben-Meir",
+            "img": jeremy,
+            "uni": "Cornell University",
+        },
+        "Ignacio": {
+            "lastname": "Lopez Gaffney",
+            "img": ignacio,
+            "uni": "Columbia University",
+        },
+        "Willie": {
+            "lastname": "Swett",
+            "img": willie,
+            "uni": "Harvard University",
+        },
+    };
+
+    const member = (name) => (
+        <div style={{width:"200px",height:"100%",flexFlow: "column", backgroundColor:"white",textAlign:"center"}}>
+            <div style={{width:"100%",height:"200px",overflow: "hidden"}}>
+                <img src={memberList[name]["img"]} alt={name} style={{borderRadius:"50%", width:"100%",height:"100%"}}/>
+            </div>
+            <div style={{marginTop:"20px"}}>
+                <a>{name + " " + memberList[name]['lastname']}</a><br/>
+                <a>{memberList[name]['uni']}</a>
+            </div>
+        </div>
+    )
+
     return (
         <div style={{width: "100%", height:"100%"}}>
-            <div style={{width: "75%", height:"200px", display: "flex", justifyContent: "space-evenly",margin:"auto",position:"absolute",top:0,bottom:0,left:0,right:0}}>
-                <div style={{width:"200px",height:"100%",overflow: "hidden",backgroundSize : "contain",objectFit:"contain"}}>
-                    <img src={jeremy} alt="Jeremy" style={{borderRadius:"50%", width:"100%",height:"100%"}}/>
-                </div>
-                <div style={{width:"200px",height:"100%",overflow: "hidden"}}>
-                    <img src={ignacio} alt="Ignacio" style={{borderRadius:"50%", width:"100%",height:"100%"}} />
-                </div>
-                <div style={{width:"200px",height:"100%",overflow: "hidden"}}>
-                    <img src={willie} alt="Willie"  style={{borderRadius: "50%", width:"100%",height:"100%"}}/>
-                </div>
+            <div style={{width: "75%", height:"300px", display: "flex", justifyContent: "space-evenly",margin:"auto",position:"absolute",top:0,bottom:0,left:0,right:0, backgroundColor:"white"}}>
+                {Object.entries(memberList).map((link, index) => ( member(link[0]) ))}
             </div>
         </div>
     );

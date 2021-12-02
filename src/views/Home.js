0fa29648d28993a20c403c78399de4693612e16c
jeremyben-meir <jsb459@cornell.edu>
2021-12-02 00:43:01 -0500

    security, error signal, temporary hard code data

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,20 @@
 import {Map} from '../components/Map.js';
 import disableScroll from 'disable-scroll';
 import React, { useState, useEffect } from "react";
+import * as LLIDPlacesJSON from "../assets/temp/llid_timeline.json";
+import * as BBLPlacesJSON from "../assets/temp/bbl_timeline.json";
 import AWS from 'aws-sdk';
 
-AWS.config.update({
-  accessKeyId: process.env.REACT_APP_ACCESS_ID,
-  secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
-});
+// AWS.config.update({
+//   accessKeyId: process.env.REACT_APP_ACCESS_ID,
+//   secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
+// });
 
 const Home = () => {
   const s3 = new AWS.S3();
-  const [BBLPlaces, setBBLPlaces] = useState(null)
+  const devSwitch = "no_pull"
+  const [errorSig, setErrorSig] = useState(null)
+  const [BBLPlaces, setBBLPlaces] = useState(false)
   const [LLIDPlaces, setLLIDPlaces] = useState(null)
 
   useEffect( () => disableScroll.on(), [] );
@@ -18,34 +22,55 @@ const Home = () => {
 
   useEffect(() => {
 
-    s3.getObject({
-        Bucket: process.env.REACT_APP_INTERNAL_BUCKET_NAME,
-        Key: 'data/bbl_timeline.json',
-    }, (err, data) => {
-        if (err) {
-            console.log(err, err.stack);
-        } else {
-            var response = JSON.parse(data.Body.toString())
-            setBBLPlaces(response)
-            s3.getObject({
-                Bucket: process.env.REACT_APP_INTERNAL_BUCKET_NAME,
-                Key: 'data/llid_timeline.json',
-            }, (err, data) => {
-                if (err) {
-                    console.log(err, err.stack);
-                } else {
-                    var response = JSON.parse(data.Body.toString())
-                    setLLIDPlaces(response)
-                }
-            });    
-        }
-    });
+    if (devSwitch == "pull"){
+      s3.getObject({
+          Bucket: process.env.REACT_APP_INTERNAL_BUCKET_NAME,
+          Key: 'data/bbl_timeline.json',
+      }, (err, data) => {
+          if (err) {
+              setErrorSig(true);
+              console.log(err, err.stack);
+          } else {
+              var response = JSON.parse(data.Body.toString())
+              setBBLPlaces(response)
+              s3.getObject({
+                  Bucket: process.env.REACT_APP_INTERNAL_BUCKET_NAME,
+                  Key: 'data/llid_timeline.json',
+              }, (err, data) => {
+                  if (err) {
+                      setErrorSig(true);
+                      console.log(err, err.stack);
+                  } else {
+                      var response = JSON.parse(data.Body.toString())
+                      setLLIDPlaces(response)
+                  }
+              });    
+          }
+      });
+    } else {
+      setBBLPlaces(BBLPlacesJSON.default)
+      setLLIDPlaces(LLIDPlacesJSON.default)
+    }
 
           
   },[])
 
+  var errorStyle = {
+    textAlign:"center",
+    lineHeight: "300px",
+    height: "100%",
+    // border: "3px solid green"
+  }
+
+  var errorDisplay = (
+    <p style={errorStyle}>Error retrieving data. Try again soon! In the meantime, visit our other pages to learn more!</p>
+  )
+  var mapDisplay = (
+    <Map BBLPlaces={BBLPlaces} LLIDPlaces={LLIDPlaces}/>
+  )
+
   return (
-    LLIDPlaces != null && <Map BBLPlaces={BBLPlaces} LLIDPlaces={LLIDPlaces}/>
+    LLIDPlaces != null ? mapDisplay : (errorSig && errorDisplay)
   );
 }
 
